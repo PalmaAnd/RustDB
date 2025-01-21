b6280ef8d799dfb3abd2df65d56a8275ac021f58
André Palma <andrepalma@outlook.de>
2025-01-21 10:43:34 +0100

    Started work on error handling

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,15 @@
-// TODO I want to handle errors here
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DBError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    
+    #[error("Page error: {0}")]
+    Page(String),
+    
+    #[error("Storage error: {0}")]
+    Storage(String),
+}
+
+pub type Result<T> = std::result::Result<T, DBError>;
